3ed9904683e8164be4fb24a59847caf2cc94996d
shaghayegh5ghasemi <52372702+shaghayegh5ghasemi@users.noreply.github.com>
2021-06-08 23:02:32 +0430

    Fuzzy C-means finished

--- C-Means.py
+++ C-Means.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 
 # x = nodes, v = centroids, u = memberships, c = number of clusters, m = a parameter > 1
@@ -12,8 +11,7 @@ def norm(node1, node2):
     return distance**0.5
 
 #Step 1: calculate membership of each node
-def membership_cal(x, v, m):
-    u = np.zeros((len(v), len(x)))
+def membership_cal(x, u, v, m):
     for i in range(len(v)):
         for k in range(len(x)):
             if norm(x[k], v[i]) != 0:
@@ -23,7 +21,10 @@ def membership_cal(x, v, m):
                 u[i, k] = 1 / sigma
             else:
                 u[i, k] = 1
-    return u
+                for j in range(len(v)):
+                    if j != i:
+                        u[j, k] = 0
+
 
 #Step 2: calculate new centroids
 def centroid_cal(x, v, u, m):
@@ -34,15 +35,14 @@ def centroid_cal(x, v, u, m):
             temp1 += ((u[i, k])**m)*x[k]
             temp2 += (u[i, k])**m
         v[i] = temp1/temp2
-    return v
 
 #Calculate the cost
 def cost(x, v, u, m):
-    j = 0
-    for k in range(len(x)):
+    cost = 0
+    for j in range(len(x)):
         for i in range(len(v)):
-            j += ((u[i][k])**m)*(norm(x[k], v[i])**2)
-    return j
+            cost += ((u[i][j])**m)*(norm(x[j], v[i])**2)
+    return cost
 
 def c_means(x, c, m):
     costs = 0
@@ -54,14 +54,26 @@ def c_means(x, c, m):
     for i in range(c):
         node = []
         for j in range(len(col_min)):
-            node.append(random.uniform(col_min[j], col_max[j]))
+            node.append(np.random.uniform(col_min[j], col_max[j]))
         v.append(node)
     # calculate membership and new centroids for 100 times
+    u = np.zeros((len(v), len(x)))
     for i in range(100):
-        u = membership_cal(x, v, m)
-        v = centroid_cal(x, v, u, m)
-        costs = cost(x, v, u, m)
+        membership_cal(x, u, v, m)
+        centroid_cal(x, v, u, m)
+    costs = cost(x, v, u, m)
+    cluster_plot(x,u,v)
     return costs
+
+#scatter points based on their membership size
+def cluster_plot(x, u, v):
+    xaxis = x[:,0]
+    yaxis = x[:,1]
+    v=np.array(v)
+    for i in range(len(v)):
+        plt.scatter(xaxis, yaxis,alpha=u[i])
+    plt.scatter(v[:,0],v[:,1],color="purple",s=200,marker='^')
+    plt.show()
 
 #read from CSV data file
 data = pd.read_csv("data1.csv", header = None)
@@ -71,7 +83,7 @@ costs = []
 for i in range(1, 7):
     costs.append(c_means(data_arr, i, 2))
 
-plt.plot(costs)
+plt.plot(list(range(1,7)), costs)
 plt.show()
 
 
